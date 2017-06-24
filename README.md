6a1c545547a2480af1621cf1f38b4df05ecb6844
Wallace <soareswallace@icloud.com>
2017-06-24 18:38:50 -0300

    Inicio

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 How to compile ?  
 gcc lock.c -fopenmp 
+
+Teorema de LaPlace  
+http://mundoeducacao.bol.uol.com.br/matematica/teorema-laplace.htm
--- ap8.c
+++ ap8.c
@@ -4,5 +4,9 @@
 
 int main(){
 	int matriz = {{5, 0, 1, 1},{-2, 3, 4, 1}, {0, 2, -1, 1}, {1, 1, 1, 1}};
-	int i, j, k;
+	int i, j, k, thread_id;
+	#pragma omp parallel for private (j, k) num_threads(4)
+	{//funfact: este pragma so funciona se as chaves estiverem assim
+			
+	}
 }
